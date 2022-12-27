c3fff11c07c1b971b454e659a0e0519c74d6cc02
Ash Porter <ashley.porter@eyecuelab.com>
2022-12-26 23:23:58 -0800

    way better variable naming

--- chronosphereMaterials.js
+++ chronosphereMaterials.js
@@ -1,17 +1,18 @@
 // To be pasted directly into the developer console while playing the game
 // ----- Get Chronosphere Count
-let chronosphereCount = game.bldTab;
-const chronoBuilding = chronosphereCount.children.filter((child) => {
+let buildingsArray = game.bldTab;
+const chronosphereObject = buildingsArray.children.filter((child) => {
   return child.model.options.building === `chronosphere`;
 });
-const chronoCount = chronoBuilding[0].model.on;
+const activeChronosphereCount = chronosphereObject[0].model.on;
 
 // ----- Get Resources
 const { resources } = game.resPool;
 let chronoData = resources
   .filter((resource) => resource.value > 0)
   .map((resource) => {
-    const chronosphere = resource.value * ((chronoCount * 1.5) / 100);
+    const chronosphere =
+      resource.value * ((activeChronosphereCount * 1.5) / 100);
     const { name, value } = resource;
     return { name, value, chronosphere };
   });
